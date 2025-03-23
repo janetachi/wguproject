5ee7313a2fda1cfc06bdf962db73ebdf8a450071
janetachi <jotien10@wgu.edu>
2025-03-22 20:23:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -149,7 +149,7 @@ int main() {
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99@yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,YourFirstName,YourLastName,YourEmail@domain.com,21,20,25,30,SOFTWARE"
+        "A5,Janet,Otieno,otienojano17@gmail.com,21,20,25,30,SOFTWARE"
     };
 
     Roster classRoster;
